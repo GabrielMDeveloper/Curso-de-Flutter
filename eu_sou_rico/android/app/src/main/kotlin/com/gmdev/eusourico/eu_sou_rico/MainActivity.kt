184e0d57390a86f2a58e99b6b3f2ab322a741043
Gabriel M <gabriel.mb280@gmail.com>
2023-05-13 17:41:25 -0300

    Primeiro Update
    Curso de Flutter, Projeto "Eu Sou Rico"

---
+++ eu_sou_rico/android/app/src/main/kotlin/com/gmdev/eusourico/eu_sou_rico/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gmdev.eusourico.eu_sou_rico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ eu_sou_rico/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //Meu Primeiro Código em Flutter.
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigoAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[900],
+          title:  Text('Eu Sou Rico'),
+        ),
+        body: Center(
+          child: Image(
+            //image: NetworkImage('https://3.bp.blogspot.com/-3iF9Ot0Bx_k/V47miTpiPgI/AAAAAAAABNE/WN4V3bP2JgE4AALmxnWAeoC6RggRrn37wCLcB/s1600/casal-de-lobos-deitados-a2df4.jpg'),
+            image: AssetImage('images/rubi.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
